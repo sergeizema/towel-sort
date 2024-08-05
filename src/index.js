9597ac8e8bc7fc12475422f6fee95dd680a7c7f4
Sergei Zemnitsky <zema.working@yandex.by>
2024-08-05 10:15:38 +0300

    feat: new index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
 // You should implement your task here.
 
+const getSortingTowel = (matrix, array, isRevers) => {
+  if(!isRevers) {
+    for(let i = 0; i < matrix.length; i++)
+      array.push(matrix[i]);
+  }
+  else {
+    for(let i = matrix.length - 1; i >= 0; i--)
+      array.push(matrix[i]);
+  }
+};
+
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArray = [];
+
+  if (matrix) {
+    matrix.forEach((item, index) => {
+      getSortingTowel(matrix[index], newArray, index % 2 == 1);
+    }); 
+  }
+ 
+  return newArray;
 }
